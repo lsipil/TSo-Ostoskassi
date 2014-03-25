50b8222aa211ba50cd4fb469e02e58665e258897
Käyttö <kaytto@dsl-hkibrasgw3-54fb71-216.dhcp.inet.fi>
2014-03-25 20:52:42 +0200

    Parantelua...

--- sql/create-tables.sql
+++ sql/create-tables.sql
@@ -1,1 +1,34 @@
+CREATE TABLE Asiakas
+(
+asiakasID SERIAL PRIMARY KEY,
+nimi varchar(60) NOT NULL
+);
 
+CREATE TABLE Lasnaolo
+(
+asiakasID integer REFERENCES Asiakas(asiakasID) on DELETE cascade,
+lennonnimi varchar(12) REFERENCES Lento(lennonnimi) on DELETE cascade
+);
+
+CREATE TABLE Lento
+(
+lennonnimi varchar(12) PRIMARY KEY,
+lahtokentta char(3) NOT NULL,
+maaranpaakentta char(3) NOT NULL,
+lahtoaika timestamp
+);
+
+CREATE TABLE Ostoskori
+(
+koriID SERIAL PRIMARY KEY,
+asiakasID integer REFERENCES Asiakas(asiakasID) on DELETE cascade,
+lukitustila boolean
+)
+;
+
+CREATE TABLE Ostos
+(
+koriID integer REFERENCES Ostoskori(koriID) on DELETE cascade,
+tuoteID integer REFERENCES Tuote(tuoteID) on DELETE cascade,
+maara integer
+)
--- sql/drop-tables.sql
+++ sql/drop-tables.sql
@@ -1,1 +1,2 @@
+DROP TABLE Asiakas, Lasnaolo, Läsnäolo, Lento, Ostoskori, Ostos, Tuote, Tuotekategoria, Valmistaja CASCADE;
 
